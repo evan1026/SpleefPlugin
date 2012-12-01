432ce37c5ad9bf726127a57ff218adf9068b9f45
Evan Allan <kkrockme4@gmail.com>
2012-11-30 21:46:15 -0500

    Version 1.0 commit
    Added saving and loading arenas. Officially commiting to version 1.0.
    Still to be added: arena based permissions.

--- src/org/noip/evan1026/SpleefPlugin.java
+++ src/org/noip/evan1026/SpleefPlugin.java
@@ -1,11 +1,18 @@
 package org.noip.evan1026;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -31,10 +38,12 @@ public class SpleefPlugin extends JavaPlugin implements Listener {
 		log = getLogger();
 		log.info("Spleef is enabled WOO!!");
 		getServer().getPluginManager().registerEvents(this, this);
+		loadFiles();
 	}
 
 	public void onDisable(){
 		log.info("Y U NO LOVE SPLEEF?!");
+		saveFiles();
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
@@ -154,7 +163,7 @@ public class SpleefPlugin extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event){
-		if (!blocks.isEmpty() && blocks.containsKey(event.getClickedBlock().getLocation()) && event.getPlayer().hasPermission("evan1026.spleef.instabreak") && event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
+		if (event.getAction().equals(Action.LEFT_CLICK_BLOCK) && blocks.containsKey(event.getClickedBlock().getLocation()) && event.getPlayer().hasPermission("evan1026.spleef.instabreak")){
 			event.getClickedBlock().setTypeId(0);
 		}
 	}
@@ -163,6 +172,68 @@ public class SpleefPlugin extends JavaPlugin implements Listener {
 	public void onBlockBreak(BlockBreakEvent event){
 		if (!blocks.isEmpty() && blocks.containsValue(event.getBlock().getLocation()) && !event.getPlayer().hasPermission("evan1026.spleef.break")){
 			event.setCancelled(true);
+		}
+	}
+	
+	private void loadFiles(){
+		List<String> file;
+		try {
+			file = Files.readAllLines(Paths.get("plugins/SpleefPlugin/arenaBlocks.txt"), Charset.defaultCharset());
+		} catch (IOException e) {
+			log.info("Save file not found. It will be created next time this plugin is disabled.");
+			return;
+		}
+		for(String x : file){
+			String arena = x.substring(0,x.indexOf(":"));
+			x = x.substring(x.indexOf(":") + 1);
+			arenaBlockLocations.put(arena, new ArrayList<Location>());
+			for(String y : x.split(";")){
+				String world = y.substring(0, y.indexOf(","));
+				y = y.substring(y.indexOf(",") + 1);
+				double locX = Double.parseDouble(y.substring(0, y.indexOf(",")));
+				y = y.substring(y.indexOf(",") + 1);
+				double locY = Double.parseDouble(y.substring(0, y.indexOf(",")));
+				y = y.substring(y.indexOf(",") + 1);
+				double locZ = Double.parseDouble(y.substring(0, y.indexOf(",")));
+				y = y.substring(y.indexOf(",") + 1);
+				int blockID = Integer.parseInt(y.substring(0, y.indexOf(",")));
+				y = y.substring(y.indexOf(",") + 1);
+				byte data = Byte.parseByte(y);
+				Location loc = new Location(getServer().getWorld(world), locX, locY, locZ);
+				arenaBlockLocations.get(arena).add(loc);
+				Block block = loc.getBlock();
+				BlockState tempState = block.getState();
+				block.setTypeId(blockID);
+				block.setData(data);
+				blocks.put(loc, block.getState());
+				block.setTypeId(tempState.getTypeId());
+				block.setData(tempState.getRawData());
+			}
+		}
+	}
+	
+	private void saveFiles(){
+		List<String> file = new ArrayList<String>();
+		File path = new File("plugins/SpleefPlugin");
+		path.mkdirs();
+		path = new File("plugins/SpleefPlugin/arenaBlocks.txt");
+		try {
+			path.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		for(String x : arenaBlockLocations.keySet()){
+			String tempString = x + ":";
+			for(Location loc : arenaBlockLocations.get(x)){
+				tempString += loc.getWorld().getName() + "," + loc.getX() + "," + loc.getY() + "," + loc.getZ() + "," + blocks.get(loc).getTypeId() + "," + blocks.get(loc).getRawData() + ";";
+			}
+			file.add(tempString.substring(0,tempString.length() - 1));
+		}
+		try {
+			Files.write(Paths.get("plugins/SpleefPlugin/arenaBlocks.txt"), file, Charset.defaultCharset());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
